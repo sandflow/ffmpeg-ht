72e30d05907b6a74d4f9722eb53362d877dab26e
caleb <etemesicaleb@gmail.com>
2022-04-25 22:05:13 +0300

    avcodec/jpeg2000: Fix Rsiz marker check.

--- libavcodec/jpeg2000dec.c
+++ libavcodec/jpeg2000dec.c
@@ -2203,7 +2203,7 @@ static int jpeg2000_read_main_headers(Jpeg2000DecoderContext *s)
             if (codsty->cblk_style & JPEG2000_CTSY_HTJ2K_F ){
                 /* Confirm Marker constraints according to Annex A of Rec. ITU-T T.814 | ISO/IEC 15444-15 */
                 // A.2
-                if ((s->avctx->profile & (1<<14))){
+                if (!(s->avctx->profile & (1<<14))){
                     av_log(s->avctx, AV_LOG_ERROR, "Bit 14 of Rsiz is not equal to 1\n");
                     return AVERROR_INVALIDDATA;
                 }
