51250c47b4c79e58a401e422a024145b0f8c3dae
caleb <etemesicaleb@gmail.com>
2022-05-17 23:46:33 +0300

    avcodec/jpeg2000: Add support for HTJ2K code streams
    C1.
    
    - Move byte unstuffer from header to .c file.
    - Create decode function to be called to decode HTJ2K code-streams
    - Get length of HT Cleanup segment(Lcup)
    -

--- libavcodec/jpeg2000_htj2k.c
+++ libavcodec/jpeg2000_htj2k.c
@@ -24,5 +24,119 @@
  * JPEG2000 High Throughtput block decoder
  */
 
-
 #include "jpeg2000_htj2k.h"
+
+static int jpeg2000_init_byte_buf(Jpeg2000ByteBuffer *buffer, GetByteContext *b)
+{
+    buffer->bit_buf = 0;
+    buffer->bits_left = 0;
+    buffer->src = b;
+    return 0;
+}
+
+static int jpeg2000_refill_and_unsfuff(Jpeg2000DecoderContext *s,Jpeg2000ByteBuffer *buffer)
+{
+    uint64_t tmp; // temporary storage for bytes
+    size_t bytes_left;
+    uint8_t initial_bits;
+
+    if (buffer->bits_left > 32) {
+        return 0; // enough data, no need to pull in more bits
+    }
+
+    bytes_left = buffer->src->buffer_end - buffer->src->buffer_start;
+
+    if (bytes_left > 4) {
+        tmp = bytestream2_get_be32u(buffer->src);
+        buffer->bits_left += 32;
+    } else if (bytes_left == 3) {
+        tmp = bytestream2_get_be24u(buffer->src);
+        buffer->bits_left += 24;
+    } else if (bytes_left == 2) {
+        tmp = bytestream2_get_be16u(buffer->src);
+        buffer->bits_left += 16;
+    } else if (bytes_left == 1) {
+        tmp = bytestream2_get_byteu(buffer->src);
+        buffer->bits_left += 8;
+    } else {
+        av_log(s->avctx, AV_LOG_ERROR, "No more bytes left\n");
+        return 1;
+    }
+
+    // check for stuff bytes (0xff)
+    if (has_byte(tmp, 0xff)) {
+        // 0xff exists, check for stuffing.
+
+        // borrowed from open_htj2k ht_block_decoding.cpp
+        // thanks Osamu Watanabe.
+
+        // Load the next byte to check for stuffing.
+        tmp <<= 8;
+        tmp = bytestream2_peek_byte(buffer->src);
+
+        if ((tmp & 0x7FFF000000) > 0x7F8F000000) {
+            tmp &= 0x7FFFFFFFFF;
+            buffer->bits_left--;
+        }
+        if ((tmp & 0x007FFF0000) > 0x007F8F0000) {
+            tmp = (tmp & 0x007FFFFFFF) + ((tmp & 0xFF00000000) >> 1);
+            buffer->bits_left--;
+        }
+        if ((tmp & 0x00007FFF00) > 0x00007F8F00) {
+            tmp = (tmp & 0x00007FFFFF) + ((tmp & 0xFFFF000000) >> 1);
+            buffer->bits_left--;
+        }
+        if ((tmp & 0x0000007FFF) > 0x0000007F8F) {
+            tmp = (tmp & 0x0000007FFF) + ((tmp & 0xFFFFFF0000) >> 1);
+            buffer->bits_left--;
+        }
+        // remove temporary byte loaded.
+        tmp >>= 8;
+    }
+    // Add bits to the MSB of the bit buffer
+    buffer->bit_buf |= (uint64_t)tmp << (64 - buffer->bits_left);
+    return 0;
+}
+
+int decode_htj2k(Jpeg2000DecoderContext *s, Jpeg2000CodingStyle *codsty, Jpeg2000T1Context *t1, Jpeg2000Cblk *cblk, int width, int height, int bandpos, uint8_t roi_shift)
+{
+    /* (cae) : Notes in no order about the code structure
+     *
+     * cblk -> Values mainly filled in  jpeg2000dec.c/jpeg2000_decode_packet.
+     *
+     * */
+    uint8_t p0 = 0;          // Number of placeholder passes.
+    uint32_t Lcup = 0;       // Length of HT cleanup segment.
+    uint32_t Lref = 0;       // Length of Refinement segment.
+    const uint8_t Sskip = 0; // Number of HT Sets preceding the given set.
+    int z_blk;               // Number of ht coding pass
+    uint8_t empty_passes;
+
+    if (cblk->npasses==0){
+        return 0;
+    }
+    av_log(s->avctx, AV_LOG_TRACE, "Initializing HTJ2K decoder\n");
+
+    if (cblk->npasses > 3)
+        // TODO:(cae) Add correct support for this
+        // Currently use this as a dummy but should be fixed soon
+        p0 = 0;
+    else if (cblk->length == 0 && cblk->npasses != 0)
+        p0 = 1;
+
+    empty_passes = p0 * 3;
+    z_blk = cblk->npasses - empty_passes;
+
+    if (z_blk <= 0)
+        // no passes within this set, continue
+        return 0;
+
+    Lcup = cblk->length;
+    if (Lcup < 2) {
+        av_log(s->avctx, AV_LOG_ERROR, "Cleanup pass length must be at least 2 bytes in length");
+        return AVERROR_INVALIDDATA;
+    }
+
+
+    return 1;
+}
--- libavcodec/jpeg2000_htj2k.h
+++ libavcodec/jpeg2000_htj2k.h
@@ -23,10 +23,10 @@
  * @file
  * JPEG2000 High Throughtput block decoder
  */
+#include "jpeg2000dec.h"
 #include <stdint.h>
 
 #include "bytestream.h"
-
 
 /**
  * Determine if a word has a zero byte
@@ -35,7 +35,8 @@
  *
  * @returns a number whose high bit is set if a byte at a position  is equal to zero
  * */
-static uint32_t has_zero(uint32_t dword){
+static uint32_t has_zero(uint32_t dword)
+{
     // Borrowed from the famous stanford bithacks page
     // see https://graphics.stanford.edu/~seander/bithacks.html#ZeroInWord
     return ~((((dword & 0x7F7F7F7F) + 0x7F7F7F7F) | dword) | 0x7F7F7F7F);
@@ -46,23 +47,19 @@ static uint32_t has_zero(uint32_t dword){
  * @param dword value to check if it contains some bytes
  * @param bytes the needle we are looking for in the haystack
  * */
-static uint32_t has_byte(uint32_t dword,uint8_t byte)
+static uint32_t has_byte(uint32_t dword, uint8_t byte)
 {
-    return has_zero(dword  ^ (~0UL/255 * (byte)));
+    return has_zero(dword ^ (~0UL / 255 * (byte)));
 }
 
-
-
-typedef  struct Jpeg2000ByteBuffer{
-    uint8_t bits_left;   //number of bits remaining in the byte buffer
-    uint64_t  buf;      // actual byte buffer
+typedef struct Jpeg2000ByteBuffer {
+    uint8_t bits_left; // number of bits remaining in the byte buffer
+    uint64_t bit_buf;  // actual byte buffer
+    GetByteContext *src; // source from which we pull our data from
 } Jpeg2000ByteBuffer;
 
-static int jpeg2000_init_byte_buf(Jpeg2000ByteBuffer *buffer){
-    buffer->bits_left=0;
-    buffer->buf = 0;
-    return 0;
-}
+/* Initialize the byte buffer for HTJ2K decoding */
+static int jpeg2000_init_byte_buf(Jpeg2000ByteBuffer *buffer, GetByteContext *b);
 
 /**
  * Refill the bit buffer with new bytes unstuffing bits if needed
@@ -70,64 +67,10 @@ static int jpeg2000_init_byte_buf(Jpeg2000ByteBuffer *buffer){
  * @param buffer THe current bit-buffer where we are adding new bits
  * @param b The source where we are pulling bits
  * */
-static int jpeg2000_refill_and_unsfuff(Jpeg2000ByteBuffer *buffer, GetByteContext *b){
-    uint64_t  tmp;  // temporary storage for bytes
-    size_t    bytes_left;
-    uint8_t   initial_bits;
-
-    if (buffer->bits_left>32){
-        return 0; // enough data, no need to pull in more bits
-    }
-    bytes_left = b->buffer_end - b->buffer_start;
-
-    if (bytes_left > 4){
-        tmp = bytestream2_get_be32u(b);
-        buffer->bits_left+=32;
-    }
-    else if (bytes_left==3){
-        tmp = bytestream2_get_be24u(b);
-        buffer->bits_left+=24;
-    }
-    else if (bytes_left==2){
-        tmp = bytestream2_get_be16u(b);
-        buffer->bits_left+=16;
-    }
-    else{
-        tmp = bytestream2_get_byteu(b);
-        buffer->bits_left+=8;
-    }
-
-    // check for stuff bytes (0xff)
-    if (has_byte(tmp, 0xff)){
-        // 0xff exists, check for stuffing.
-
-        // borrowed from open_htj2k ht_block_decoding.cpp
-        // thanks Osamu Watanabe.
-
-        // Load the next byte to check for stuffing.
-        tmp <<= 8;
-        tmp = bytestream2_peek_byte(b);
-
-        if ((tmp & 0x7FFF000000) > 0x7F8F000000) {
-            tmp &= 0x7FFFFFFFFF;
-            buffer->bits_left--;
-        }
-        if ((tmp & 0x007FFF0000) > 0x007F8F0000) {
-            tmp = (tmp & 0x007FFFFFFF) + ((tmp & 0xFF00000000) >> 1);
-            buffer->bits_left--;
-        }
-        if ((tmp & 0x00007FFF00) > 0x00007F8F00) {
-            tmp = (tmp & 0x00007FFFFF) + ((tmp & 0xFFFF000000) >> 1);
-            buffer->bits_left--;
-        }
-        if ((tmp & 0x0000007FFF) > 0x0000007F8F) {
-            tmp = (tmp & 0x0000007FFF) + ((tmp & 0xFFFFFF0000) >> 1);
-            buffer->bits_left--;
-        }
-        // remove temporary byte loaded.
-        tmp>>=8;
-    }
-    // Add bits to the MSB of the bit buffer
-    buffer->buf |= (uint64_t)tmp << (64- buffer->bits_left);
-    return 1;
-}
+static int jpeg2000_refill_and_unsfuff( Jpeg2000DecoderContext *s,Jpeg2000ByteBuffer *buffer);
+/**
+ * Decode a jpeg2000 High throughtput bitstream
+ *
+ * @returns 0 on success, all other values are errors.
+ * */
+int decode_htj2k(Jpeg2000DecoderContext *s, Jpeg2000CodingStyle *codsty, Jpeg2000T1Context *t1, Jpeg2000Cblk *cblk, int width, int height, int bandpos, uint8_t roi_shift);
