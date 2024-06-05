f84719fdadf3798c67a5d23a04d21f8646690d90
Osamu Watanabe <65328111+osamu620@users.noreply.github.com>
2024-06-05 18:04:59 +0900

    Revert changes on select_header()
    It was found that the change on select_header() was not necessary because this function should only be called when the PPM marker is present. Packed packet header is allowed only in main header when PPM is used. 
    
    (See A.7.4 in the Part 1 spec; "The packet headers shall be in only one of three places within the codestream. If the PPM marker segment is present, all
    the packet headers shall be found in the main header. In this case, the PPT marker segment and packets distributed in the
    bit stream of the tile-parts are disallowed.")
    
    So, the modification of this function can be reverted without any regression.

--- libavcodec/jpeg2000dec.c
+++ libavcodec/jpeg2000dec.c
@@ -1003,18 +1003,11 @@ static int getlblockinc(Jpeg2000DecoderContext *s)
 static inline void select_header(Jpeg2000DecoderContext *s, const Jpeg2000Tile *tile,
                                  int *tp_index)
 {
-    int32_t is_endof_tp;
-
     s->g = tile->tile_part[*tp_index].header_tpg;
-    is_endof_tp = bytestream2_get_bytes_left(&s->g) == 0 && s->bit_index == 8;
-    // Following while loop is necessary because a tilepart may include only SOD marker.
-    // Such a tilepart has neither packet header nor compressed data.
-    while (is_endof_tp) {
+    if (bytestream2_get_bytes_left(&s->g) == 0 && s->bit_index == 8) {
+        av_log(s->avctx, AV_LOG_WARNING, "Packet header bytes in PPM marker segment is too short.\n");
         if (*tp_index < FF_ARRAY_ELEMS(tile->tile_part) - 1) {
-            s->g = tile->tile_part[++(*tp_index)].header_tpg;
-            is_endof_tp = bytestream2_get_bytes_left(&s->g) == 0 && s->bit_index == 8;
-        } else {
-            is_endof_tp = 0;
+            s->g = tile->tile_part[++(*tp_index)].tpg;
         }
     }
 }
